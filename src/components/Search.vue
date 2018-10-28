cc1730983b62fd705b2ab74ccd6761f6fae978f5
Magnus <magnus.grande@gmail.com>
2018-10-28 21:11:39 +0100

    fixed issue where date would be incremented for every rest call

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search">
     <input class="search-input" v-model="input">
-    <button class="submit-button" @click="test"></button>
+    <button class="submit-button" @click="find"></button>
   </div>
 </template>
 
@@ -20,22 +20,24 @@ export default {
     }
   },
   methods: {
-    test () {
+    find () {
+      this.now = this.$moment()
       let from = this.now.format('YYYY-MM-DD')
       let to = this.now.add(1, 'days').format('YYYY-MM-DD')
       this.$http.get('https://maps.googleapis.com/maps/api/geocode/json?address=Geiranger,+CA&key=AIzaSyDpURb79tdvjDP5GEdlhBKJFXD6a3qWMRc').then((res) => {
         this.lat = res.data.results[0].geometry.location.lat
         this.lon = res.data.results[0].geometry.location.lng
         this.$http.get('https://api.sehavniva.no/tideapi.php?lat=' + this.lat + '&lon=' + this.lon + '&fromtime=' + from + 'T00%3A00&totime=' + to + 'T00%3A00&datatype=all&refcode=cd&place=&file=&lang=nn&interval=60&dst=0&tzone=&tide_request=locationdata').then((res) => {
-          parseString(res.data, (err, result) => {
-            this.$store.commit('setTide', result.tide.locationdata[0].data[0].waterlevel)
+          parseString(res.data, {trim: true}, (err, result) => {
+            result.tide.locationdata[0].data.forEach((el) => {
+              if (el.$.type === 'forecast') {
+                this.$store.commit('setTide', el.waterlevel)
+              }
+            })
           })
         })
       })
     }
-  },
-  mounted () {
-    this.now = this.$moment()
   }
 }
 </script>
